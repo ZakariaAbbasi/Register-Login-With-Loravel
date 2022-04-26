3e294b28b0b30f5e0cc31f241b7c525110dd4696
Zakaria Abbasi <zakaria.abbasi75@gmail.com>
2022-04-26 14:32:28 +0430

    chenge-logo.site

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light ">
     <a class="navbar-brand" href="#">
-        <img src="{{ asset('img/logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="{{ asset('img/logo.jpeg') }}" width="30" height="30" class="d-inline-block align-top" alt="">
         آکادمی
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
